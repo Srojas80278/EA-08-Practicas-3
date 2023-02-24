6f85ccf39a55e12be33c7a3c7097d68bc3e8a29f
sroja <sroja@HP_PAVILION>
2023-02-23 20:35:35 -0600

    Creación de la practica de examen 1

---
+++ src/test/java/com/practicaexamen/PracticaExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicaexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
