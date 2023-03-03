3248619280afbe638c06a234832b7200dededd71
sroja <sroja@HP_PAVILION>
2023-03-02 21:03:49 -0600

    Práctica de examen finalizada

--- src/main/java/com/practicaexamen/controller/ConciertoController.java
+++ src/main/java/com/practicaexamen/controller/ConciertoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -47,4 +48,11 @@ public class ConciertoController {
         return "redirect:/concierto";
     }
 */
+    
+    @GetMapping("/editConcierto/{id}")
+    public String editarConcierto(@PathVariable("id") Long idConcierto, Model model){
+        Concierto concierto= conciertoService.getConciertoById(idConcierto);
+        model.addAttribute("concierto", concierto); 
+        return "crear"; 
+    }
 }
